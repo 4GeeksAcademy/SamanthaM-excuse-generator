286729182fc1b4d69d076b82e69fb3d7703859d6
Samantha McCabe <156986930+SamanthaMcCabe2104@users.noreply.github.com>
2024-02-19 21:17:16 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,40 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+/* eslint-disable */
+import "bootstrap";
+import "./style.css";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = generateExcuse();
+  });
+
+  console.log("Hello");
+};
+
+let generateExcuse = () => {
+  let Article = ["The", "A"];
+  let Subject = ["teacher", "dog", "girl", "boy"];
+  let Action = ["broke my", "ate my", "yelled at my", "burned my"];
+  let Posession = ["phone", "homework", "lunch", "book"];
+  let Time = ["at work", "during class", "at the party", "in my house"];
+
+  let ArtIndx = Math.floor(Math.random() * Article.length);
+  let SubIndx = Math.floor(Math.random() * Subject.length);
+  let ActIndex = Math.floor(Math.random() * Action.length);
+  let PosIndex = Math.floor(Math.random() * Posession.length);
+  let TimIndex = Math.floor(Math.random() * Time.length);
+
+  return (
+    Article(ArtIndx) +
+    "" +
+    Subject(SubIndx) +
+    "" +
+    Action(ActIndex) +
+    "" +
+    Posession(PosIndex) +
+    "" +
+    Time(TimIndex)
+  );
 };
